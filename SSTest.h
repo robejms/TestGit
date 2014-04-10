9e60528d7197158c3efc8134e73223f7a4eb2378
Robert James <robertjames@roberts-mbp.home>
2014-04-10 20:03:32 +0100

    added test class

---
+++ SSTest.h
@@ -0,0 +1,13 @@
+//
+//  SSTest.h
+//  GitTest
+//
+//  Created by Robert James on 10/04/2014.
+//  Copyright (c) 2014 Robert James. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSTest : NSObject
+
+@end
---
+++ SSTest.m
@@ -0,0 +1,13 @@
+//
+//  SSTest.m
+//  GitTest
+//
+//  Created by Robert James on 10/04/2014.
+//  Copyright (c) 2014 Robert James. All rights reserved.
+//
+
+#import "SSTest.h"
+
+@implementation SSTest
+
+@end
